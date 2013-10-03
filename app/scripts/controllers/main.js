074db7b548c883c9ebc1d33dcd3ec720c33ba6b4
Julian Tung <julian.jl.tung@gmail.com>
2013-10-03 11:31:06 +0800

    initial commit

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,10 @@
+'use strict';
+
+angular.module('HKWDIProject1GroupApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
